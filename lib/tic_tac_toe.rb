db2e45f0ff460c4b64aad75d93168e940fd4bd89
Lucas Souza <souza.lucas01@gmail.com>
2020-03-14 15:34:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -123,6 +123,16 @@ end
 
 #play_method
   def play(board)
+    display_board(board)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    index = input_to_index(user_input)
+    if valid_move?(board, index)
+      move(board, index, user_input = "X")
+      display_board(board)
+    else
+      turn(board)
+    end
      until over?(board)
         turn(board)
      end
